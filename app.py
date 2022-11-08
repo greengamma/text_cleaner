25ef7d4535236b1dd0eec2597dd11283f00cb0fa
Rene Kumsta <rck@sequensis.de>
2022-11-08 17:41:32 +0100

    Finished basic functionality

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import streamlit as st
 import neattext.functions as nfx
+import base64
+import time
+timestr = time.strftime('%Y%m%d-%H%M%S')
+
+def text_downloader(raw_text):
+    b64 = base64.b64encode(raw_text.encode()).decode()
+    new_filename = 'clean_text_result_{}_.txt'.format(timestr)
+    st.markdown('### Download File ###')
+    href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">click here</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 
 def main():
@@ -11,15 +21,56 @@ def main():
 
     if choice == 'TextCleaner':
         st.subheader('Text Cleaning')
-        text_file = st.file_uploader('Upload Txt File', type=['txt'])
-        if text_file is not None:
+        text_file = st.file_uploader('Upload .txt File', type=['txt'])
+        normalise_case = st.sidebar.checkbox('Normalised Case')
+        clean_stopwords = st.sidebar.checkbox('Stopwords')
+        clean_punctuation = st.sidebar.checkbox('Punctuations')
+        clean_emails = st.sidebar.checkbox('Emails')
+        clean_special_char = st.sidebar.checkbox('Special Characters')
+        clean_numbers = st.sidebar.checkbox('Numbers')
+        clean_urls = st.sidebar.checkbox('URLs')
 
-            col1, col2 = st.beta_columns(2)
+        if text_file is not None:
+            file_details = {'File name': text_file.name,
+                            'File size': text_file.size,
+                            'File type': text_file.type}
+            st.write(file_details)
+
+            # Decode the text
+            raw_text = text_file.read().decode('utf-8')
+
+            col1, col2 = st.columns(2)
 
             with col1:
-                with st.beta_expander('Original Text'):
-                    raw_text = text_file.read().decode('utf-8')
-                    st.write(text_file)
+                with st.expander('Original Text'):
+                    st.write(raw_text)
+
+            with col2:
+                with st.expander('Processed Text'):
+                    if normalise_case:
+                        raw_text = raw_text.upper()
+
+                    if clean_stopwords:
+                        raw_text = nfx.remove_stopwords(raw_text)
+
+                    if clean_numbers:
+                        raw_text = nfx.remove_numbers(raw_text)
+
+                    if clean_urls:
+                        raw_text = nfx.remove_urls(raw_text)
+
+                    if clean_punctuation:
+                        raw_text = nfx.remove_punctuations(raw_text)
+
+                    if clean_emails:
+                        raw_text = nfx.remove_emails(raw_text)
+
+                    if clean_special_char:
+                        raw_text = nfx.remove_special_characters(raw_text)
+
+                    st.write(raw_text)
+                    text_downloader(raw_text)
+
 
 
 
